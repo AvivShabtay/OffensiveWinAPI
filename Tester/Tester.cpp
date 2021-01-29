5c4191e3272dff25f1fd784e0927e54a692916f4
Aviv <shabtay.aviv@gmail.com>
2021-01-29 12:30:37 +0200

    Update usage of ProcessUtils
    * fix: Included headers
    * feat: Iteration using c++17 style

--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -1,21 +1,20 @@
-#include "..\Helper\common.h"
+#include <Windows.h>
 #include <iostream>
+#include <map>
+#include "..\Helper\ProcessUtils.h"
 
 int main() {
-	std::vector<std::pair<DWORD, WCHAR[260]>> procList;
-	WCHAR procName[] = L"csrss.exe";
-	DWORD pid;
+	std::map<DWORD, std::wstring> procList;
 
-	BOOL result = Common::GetAllRunningProcesses(procList);
-	if (result) {
-		for (DWORD i = 0; i < procList.size(); i++) {
-			DWORD procID = procList.at(i).first;
-			std::wstring procName(procList.at(i).second);
-			std::wcout << procID << ", " << procName << std::endl;
+	ProcessUtils::GetAllRunningProcesses(procList);
+	if (0 < procList.size()) {
+		for (auto& [pid, procName] : procList) {
+			std::wcout << pid << ", " << procName << std::endl;
 		}
 	}
 
-	pid = Common::FindPidByProcessName(procName);
+	std::wstring procName = L"csrss.exe";
+	DWORD pid = ProcessUtils::FindPidByProcessName(procName);
 	std::wcout << "PID of " << procName << " is: " << pid << std::endl;
 
 	return 0;
