799f3f5b273892888c6e8e2d02c65e1e69a58bf3
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-02-28 23:09:56 +0200

    Add helper class for Win32 exceptions

---
+++ Common/Win32ErrorCodeException.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdexcept>
+#include <iostream>
+
+#include <Windows.h>
+
+class Win32ErrorCodeException : public std::runtime_error
+{
+public:
+	explicit Win32ErrorCodeException(const std::string& errorMessage);
+
+	virtual ~Win32ErrorCodeException() = default;
+
+	const char* what() const override;
+
+	// delete copy ctor, move ctor, assignment
+	Win32ErrorCodeException(const Win32ErrorCodeException&) = delete;
+	Win32ErrorCodeException& operator=(const Win32ErrorCodeException&) = delete;
+	Win32ErrorCodeException(Win32ErrorCodeException&&) = delete;
+	Win32ErrorCodeException& operator=(Win32ErrorCodeException&&) = delete;
+
+	/* Get Windows last error code. */
+	DWORD getErrorCode() const;
+
+	/* Get Windows message corresponding to the last error that occurred. */
+	std::string getWinErrorMessage() const;
+
+protected:
+	std::string getLastErrorMessage() const;
+
+	DWORD m_errorCode;
+	std::string m_winErrorMessage;
+};
+
