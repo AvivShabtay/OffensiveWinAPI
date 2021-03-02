62bb55aa31d6e9a34434e19687e892e73c298528
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-03-02 17:50:47 +0200

    Update main function
    * feat: Use iterator for querying virtual memory
    * feat: Replace auto to type for better code clarity
    * fix: Brackets convention
    * fix: Remove redundant includes
    * fix: Remove redundant Macro
    * chore: Move helper function for main file
    * chore: Replace printf()

--- ScanMemory/main.cpp
+++ ScanMemory/main.cpp
@@ -9,27 +9,28 @@
 
 #include "../Common/AutoHandle.h"
 #include "../Common/Win32ErrorCodeException.h"
+#include "MemoryInformation.h"
 
 #include <iostream>
+#include <vector>
 #include <stdexcept>
-
 #include <atlstr.h>
 
 #include <Psapi.h>
 #include <Windows.h>
 
-// Helper function to test NTSTATUS values:
-#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
 
-using pNtSuspendProcess = NTSTATUS(NTAPI*)(IN HANDLE);
-//BOOL SuspendProc(DWORD dwPid);
-
-// Helper functions:
+/* Convert memory type mask into common name. */
 std::wstring GetMemoryType(DWORD type);
+
+/* Convert memory state mask into common name. */
 std::wstring GetMemoryState(DWORD type);
+
+/* Convert memory protection mask into common name. */
 std::wstring GetMemoryProtection(DWORD type);
 
-int wmain(int argc, PWCHAR argv[]) {
+int wmain(int argc, PWCHAR argv[])
+{
 
 	if (argc < 2)
 	{
@@ -48,42 +49,23 @@ int wmain(int argc, PWCHAR argv[]) {
 			throw Win32ErrorCodeException("Could not open handle to process");
 		}
 
-		// Query memory information of another process:
-		LPVOID lpAddress = nullptr;
-		MEMORY_BASIC_INFORMATION memoryInfo = { 0 };
-		if (0 == VirtualQueryEx(process.get(), lpAddress, &memoryInfo, sizeof(memoryInfo)))
+		MemoryInformation memoryInformation(processPid);
+		for (const MEMORY_BASIC_INFORMATION& memory : memoryInformation)
 		{
-			throw Win32ErrorCodeException("Could read process virtual memory");
-		}
+			std::vector<WCHAR> filenameData(MAX_PATH);
+			std::wstring filename(L"No mapped file");
 
-		// Setup pointers for iteration:
-		PVOID pBaseAddress = memoryInfo.AllocationBase;
-		PVOID pAddressBlock = pBaseAddress;
-
-		printf("[+] Print the process memory regions with mapped file:\n");
-
-		// Find all the mapped modules used by the process:
-		while (TRUE) {
-
-			// Get memory info about the current block of memory
-			if (VirtualQueryEx(process.get(), pAddressBlock, &memoryInfo, sizeof(memoryInfo)) != sizeof(memoryInfo)) {
-				break;
+			if (GetMappedFileName(process.get(), memory.BaseAddress, &filenameData[0], MAX_PATH))
+			{
+				filename = static_cast<WCHAR*>(filenameData.data());
 			}
 
-			// Try to get mapped file name to the memory range:
-			WCHAR fileName[MAX_PATH];
-			if (GetMappedFileName(process.get(), memoryInfo.BaseAddress, fileName, MAX_PATH)) {
+			std::wstring type = GetMemoryType(memory.Type);
+			std::wstring state = GetMemoryState(memory.State);
+			std::wstring protection = GetMemoryProtection(memory.Protect);
 
-				auto type = GetMemoryType(memoryInfo.Type);
-				auto state = GetMemoryState(memoryInfo.State);
-				auto protection = GetMemoryProtection(memoryInfo.Protect);
-
-				printf("[*] Address: 0x%08p | Base: 0x%08p | Type: %-6ws | State: %-7ws | Protection: %-3ws | FileName: %ws\n",
-					pAddressBlock, memoryInfo.BaseAddress, type.c_str(), state.c_str(), protection.c_str(), fileName);
-			}
-
-			// Move to the next memory range:
-			pAddressBlock = (PVOID)((PBYTE)pAddressBlock + memoryInfo.RegionSize);
+			printf("[*] Base: 0x%08p | Type: %-6ws | State: %-5ws | Protection: %-3ws | FileName: %ws\n",
+				memory.BaseAddress, type.c_str(), state.c_str(), protection.c_str(), filename.c_str());
 		}
 	}
 	catch (std::exception& exception)
@@ -93,61 +75,116 @@ int wmain(int argc, PWCHAR argv[]) {
 	}
 }
 
-/* Convert memory type mask into common name. */
-std::wstring GetMemoryType(DWORD type) {
+std::wstring GetMemoryType(DWORD type)
+{
+	if (type & MEM_COMMIT)
+	{
+		return L"Commit";
+	}
+
+	if (type & MEM_RESERVE)
+	{
+		return L"Reserve";
+	}
+
 	if (type & MEM_IMAGE)
+	{
 		return L"Image";
+	}
 
 	else if (type & MEM_MAPPED)
+	{
 		return L"Mapped";
+	}
 
 	else if (type & MEM_PRIVATE)
+	{
 		return L"Private";
-	else
-		return L"";
-}
-
-/* Convert memory state mask into common name. */
-std::wstring GetMemoryState(DWORD type) {
-	if (type & MEM_COMMIT)
-		return L"Commit";
+	}
 
 	else if (type & MEM_FREE)
+	{
 		return L"Free";
+	}
 
-	else if (type & MEM_RESERVE)
-		return L"Reserve";
+	else if (type == 0)
+	{
+		return L"Unusable"; // Process Hacker
+	}
+
 	else
+	{
 		return L"";
+	}
 }
 
-/* Convert memory protection mask into common name. */
-std::wstring GetMemoryProtection(DWORD type) {
+std::wstring GetMemoryState(DWORD type)
+{
+	if (type & MEM_COMMIT)
+	{
+		return L"Commit";
+	}
 
-	if (type & PAGE_READONLY)
-		return L"R";
+	else if (type & MEM_FREE)
+	{
+		return L"Free";
+	}
 
-	else if (type & PAGE_WRITECOPY)
-		return L"WC";
-
-	else if (type & PAGE_READWRITE)
-		return L"RW";
-
-	else if (type & PAGE_NOACCESS)
-		return L"NA";
-
-	else if (type & PAGE_EXECUTE)
-		return L"X";
-
-	else if (type & PAGE_EXECUTE_READ)
-		return L"RX";
-
-	else if (type & PAGE_EXECUTE_READWRITE)
-		return L"RWX";
-
-	else if (type & PAGE_EXECUTE_WRITECOPY)
-		return L"WCX";
+	else if (type & MEM_RESERVE)
+	{
+		return L"Reserve";
+	}
 
 	else
+	{
 		return L"";
+	}
+}
+
+std::wstring GetMemoryProtection(DWORD type)
+{
+	if (type & PAGE_READONLY)
+	{
+		return L"R";
+	}
+
+	else if (type & PAGE_WRITECOPY)
+	{
+		return L"WC";
+	}
+
+	else if (type & PAGE_READWRITE)
+	{
+		return L"RW";
+	}
+
+	else if (type & PAGE_NOACCESS)
+	{
+		return L"NA";
+	}
+
+	else if (type & PAGE_EXECUTE)
+	{
+		return L"X";
+	}
+
+	else if (type & PAGE_EXECUTE_READ)
+	{
+		return L"RX";
+	}
+
+	else if (type & PAGE_EXECUTE_READWRITE)
+	{
+		return L"RWX";
+	}
+
+	else if (type & PAGE_EXECUTE_WRITECOPY)
+	{
+		return L"WCX";
+	}
+
+	else
+	{
+		return L"";
+	}
 }
