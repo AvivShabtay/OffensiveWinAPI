a3fbae85c824b90cc71a1b49762eabbd7f8a2c63
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-04-09 17:36:25 +0300

    Fix constant names

--- Common/Wmi.h
+++ Common/Wmi.h
@@ -4,6 +4,7 @@
 
 // WMI library file:
 #pragma comment(lib, "wbemuuid.lib")
-const std::wstring WMI_NAMESPACE(L"ROOT\\CIMV2");
-const std::wstring WMI_QUERY_LANGUAGE(L"WQL");
+
+const std::wstring WMI_ROOT_CIMV2_NAMESPACE(L"ROOT\\CIMV2");
+const std::wstring WMI_WQL_QUERY_LANGUAGE(L"WQL");
 const std::wstring ALL_RUNNING_PROCESSES_QUERY(L"SELECT * FROM Win32_Process");
